72aab36818f4452e7d174c2cf5efbbd30d07c648
Avizhonis <aviz11@bk.ru>
2024-05-30 14:51:18 +0300

    add project structure

--- src/core/main.py
+++ src/core/main.py
@@ -1,8 +1,7 @@
-from typing import Union
-from src.routers.test_docker_routers import docker_router
+from src.core.routers.test_docker_routers import docker_router
 from fastapi import FastAPI
 
-from src.routers.healthcheck import router as health_router
+from src.core.routers.healthcheck import router as health_router
 
 app = FastAPI()
 app.include_router(health_router)
@@ -12,5 +11,3 @@ app.include_router(docker_router, prefix="/api/v1")
 @app.post("/auth/signup")
 def signup():
     return {"Hello": "World"}
-
-
