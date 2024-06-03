9678995b32a85bf5cefc3af03e75791087e5b189
Avizhonis <aviz11@bk.ru>
2024-06-03 15:25:04 +0300

    update configurations from db

--- src/core/database/database.py
+++ src/core/database/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 from src.core.config import settings
 
-engine = create_async_engine(settings.DB_URL, echo=True)
+engine = create_async_engine(settings.db.DB_URL, echo=True)
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 
