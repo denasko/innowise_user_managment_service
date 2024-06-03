1416711c725419721f81497038f2e5f1872a022d
Avizhonis <aviz11@bk.ru>
2024-06-03 15:24:20 +0300

    update configurations from db

--- src/core/config.py
+++ src/core/config.py
@@ -1,7 +1,17 @@
+from pathlib import Path
+
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings
 
 
-class Settings(BaseSettings):
+class AuthJWT(BaseModel):
+    private_key_path: Path = Path("src") / "core" / "auth" / "sertificats" / "jwt-private.pem"
+    public_key_path: Path = Path("src") / "core" / "auth" / "sertificats" / "jwt-public.pem"
+    algorithm: str = "RS256"
+    access_token_time_to_live: int = 3
+
+
+class DatabaseSettings(BaseModel):
     DB_HOST: str = "localhost"
     DB_PORT: int = 5432
     DB_NAME: str = "db_user_management"
@@ -15,6 +25,11 @@ class Settings(BaseSettings):
     @property
     def DB_URL(self) -> str:
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+
+class Settings(BaseSettings):
+    db: DatabaseSettings = DatabaseSettings()
+    jwt: AuthJWT = AuthJWT()
 
 
 settings = Settings()
