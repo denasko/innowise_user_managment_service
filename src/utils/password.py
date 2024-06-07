b075c3019d4da2b9daa9695abed6556e05de11bc
Avizhonis <aviz11@bk.ru>
2024-06-07 17:46:40 +0300

    add password file into utils directory

--- src/utils/password.py
+++ src/utils/password.py
@@ -8,6 +8,4 @@ def hash_password(password: str) -> str:
 
 
 def check_password(password: str, hashed_password: str) -> bool:
-    print(password)
-    print(hashed_password)
     return bcrypt.checkpw(password=password.encode(), hashed_password=hashed_password.encode())
