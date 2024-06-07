3f7a2c6d57313c30ec469fa16da2e04ab2c3a987
Avizhonis <aviz11@bk.ru>
2024-06-07 17:54:51 +0300

    create dependencies

---
+++ src/core/dependencies.py
@@ -0,0 +1,21 @@
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.core.database.database import get_session
+from src.services.authorization_service import AuthService
+from src.services.user_service import UserService
+from src.services.token_sevice import TokenService
+
+
+def get_user_service(session: AsyncSession = Depends(get_session)) -> UserService:
+    return UserService(session=session)
+
+
+def get_token_service() -> TokenService:
+    return TokenService()
+
+
+def get_authorization_service(
+    session: AsyncSession = Depends(get_session),
+) -> AuthService:
+    return AuthService(session=session)
