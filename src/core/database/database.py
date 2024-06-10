f9a839e84a6c569d9ec5038a80e816d1434e9b47
Avizhonis <aviz11@bk.ru>
2024-06-10 16:05:50 +0300

    update dependencies

--- src/core/database/database.py
+++ src/core/database/database.py
@@ -5,8 +5,3 @@ from src.core.config import settings
 
 engine = create_async_engine(settings.db.DB_URL, echo=True)
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
-
-
-async def get_session() -> AsyncSession:
-    async with async_session() as session:
-        yield session
--- src/core/dependencies.py
+++ src/core/dependencies.py
@@ -1,10 +1,15 @@
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.core.database.database import get_session
+from src.core.database.database import async_session
 from src.services.authorization_service import AuthService
 from src.services.user_service import UserService
 from src.services.token_sevice import TokenService
+
+
+async def get_session() -> AsyncSession:
+    async with async_session() as session:
+        yield session
 
 
 def get_user_service(session: AsyncSession = Depends(get_session)) -> UserService:
