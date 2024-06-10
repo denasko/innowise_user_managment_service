e0816913f5037d5c7cd6b404d75f0f7385f49bf6
Avizhonis <aviz11@bk.ru>
2024-06-10 16:08:52 +0300

    update read and update user endpoints

--- src/api/v1/user/read_current_user_endpoint.py
+++ src/api/v1/user/read_current_user_endpoint.py
@@ -1,15 +1,16 @@
 from fastapi import Depends
 from fastapi.security import HTTPAuthorizationCredentials
 from src.api.v1.bearer import bearer
+from src.core.database.models.user import User
 from src.core.dependencies import get_user_service
 from src.core.schemas.user import UserRead
 from src.services.user_service import UserService
 from src.api.v1.user import user_router
 
 
-@user_router.get("/me")
+@user_router.get("/me", response_model=UserRead)
 async def read_current_user(
     user_service: UserService = Depends(get_user_service),
     credentials: HTTPAuthorizationCredentials = Depends(bearer),
-) -> UserRead:
+) -> User:
     return await user_service.read_current_user(credentials=credentials)
