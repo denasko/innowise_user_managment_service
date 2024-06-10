1105e85f977c86d6de0b450250f172474803504f
Avizhonis <aviz11@bk.ru>
2024-06-10 16:07:25 +0300

    update auth service

--- src/services/authorization_service.py
+++ src/services/authorization_service.py
@@ -2,6 +2,8 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPAuthorizationCredentials
 from jwt.exceptions import InvalidTokenError
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.core.database.enums.token import TokenType
 from src.core.database.models.user import User as UserModel
 from src.api.v1.bearer import bearer
 from src.core.schemas.token import TokenInfo
@@ -16,18 +18,17 @@ class AuthService:
         self.token_service = TokenService()
 
     async def validate_auth_user(self, username: str, password: str) -> UserModel | None:
+        """return user if user exist, not blocked and password is correct"""
         user: UserModel | None = await self.repository.get_user_by_field(username=username)
-        if user is None:
+        if user is None or not check_password(password=password, hashed_password=user.password):
             return None
-        if not check_password(password=password, hashed_password=user.password):
-            return None
-
         return user
 
-    async def get_current_token_payload(
+    def get_current_token_payload(
         self,
         credentials: HTTPAuthorizationCredentials = Depends(bearer),
-    ) -> UserModel:
+    ) -> dict:
+        """return payload if token is valid"""
         token: str = credentials.credentials
         try:
             payload: dict = self.token_service.decode_jwt(token=token)
@@ -37,17 +38,19 @@ class AuthService:
                 detail=f"invalid token error: {e}",
             )
 
-        return await self.get_current_auth_not_blocked_user(payload=payload)
+        return payload
 
     async def get_current_auth_not_blocked_user(
         self,
         payload: dict,
     ) -> UserModel:
+        """return user if user exist, not blocked and type token is access"""
+
         token_type = payload.get("type")
-        if token_type != "access_token":
+        if token_type != TokenType.ACCESS:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="invalid token type")
 
-        user: UserModel | None = await self.repository.get_user_by_field(username=payload["sub"])
+        user: UserModel | None = await self.repository.get_user_by_field(user_id=payload["sub"])
         if user is None:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="user not found")
         if user.is_blocked:
@@ -56,6 +59,7 @@ class AuthService:
         return user
 
     async def login(self, username: str, password: str) -> TokenInfo:
+        """if user exist and not blocked, generate access and refresh token"""
         user: UserModel | None = await self.validate_auth_user(username=username, password=password)
         if not user:
             raise HTTPException(
