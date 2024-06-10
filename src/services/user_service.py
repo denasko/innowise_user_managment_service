60f0dda3a91d93430fc1240a8506f41db6093462
Avizhonis <aviz11@bk.ru>
2024-06-10 16:07:40 +0300

    update user service

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -1,5 +1,3 @@
-from fastapi import HTTPException, status
-
 from fastapi.security import HTTPAuthorizationCredentials
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.core.schemas.user import UserCreate, UserUpdate
@@ -16,25 +14,25 @@ class UserService:
         self.repository = UserManager(session=session)
         self.token_service = TokenService()
 
-    async def create_new_user(self, new_user: UserCreate) -> UserCreate:
+    async def create_new_user(self, new_user: UserCreate) -> UserModel:
         new_user.password = hash_password(new_user.password)
+
         return await self.repository.create_user(new_user=new_user)
 
     async def delete_user(self, credentials: HTTPAuthorizationCredentials) -> dict:
-        user_to_delete: UserModel = await self.auth_service.get_current_token_payload(credentials=credentials)
+        payload: dict = self.auth_service.get_current_token_payload(credentials=credentials)
+        user_to_delete: UserModel = await self.auth_service.get_current_auth_not_blocked_user(payload=payload)
 
         return await self.repository.delete_user(user_to_delete=user_to_delete)
 
     async def update_user(self, user_update: UserUpdate, credentials: HTTPAuthorizationCredentials) -> UserModel:
-        current_user: UserModel = await self.auth_service.get_current_token_payload(credentials=credentials)
-
-        if current_user is None:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="incorrect username or password",
-            )
+        payload: dict = self.auth_service.get_current_token_payload(credentials=credentials)
+        current_user: UserModel = await self.auth_service.get_current_auth_not_blocked_user(payload=payload)
 
         return await self.repository.edit_user(user_update=user_update, current_user=current_user)
 
-    async def read_current_user(self, credentials: HTTPAuthorizationCredentials):
-        return await self.auth_service.get_current_token_payload(credentials=credentials)
+    async def read_current_user(self, credentials: HTTPAuthorizationCredentials) -> UserModel:
+        payload: dict = self.auth_service.get_current_token_payload(credentials=credentials)
+        current_user: UserModel = await self.auth_service.get_current_auth_not_blocked_user(payload=payload)
+
+        return current_user
