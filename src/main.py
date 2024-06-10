e632a51cc6edab544a6fe9c17f64b7032544c679
Avizhonis <aviz11@bk.ru>
2024-06-10 16:01:46 +0300

    main file

--- src/main.py
+++ src/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
-from .api.healthcheck import router as health_router
-from .api import api_router as api_v1_router
+from src.api.healthcheck import router as health_router
+from src.api import api_router
 
 app = FastAPI()
 
-
 app.include_router(health_router)
-app.include_router(api_v1_router)
+app.include_router(api_router)
