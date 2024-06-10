d35e02fb577aed7ea895878733b9afeca022d7bc
Avizhonis <aviz11@bk.ru>
2024-06-10 16:45:41 +0300

    deleted files

--- alembic/env.py
+++ alembic/env.py
@@ -7,6 +7,7 @@ from src.core.database.models.base import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
+
 config = alembic.context.config
 
 # Interpret the config file for Python logging.
