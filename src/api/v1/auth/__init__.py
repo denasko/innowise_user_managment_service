7951f5d57adfff48bf1235be3d24ad1c0bf31c72
Avizhonis <aviz11@bk.ru>
2024-06-12 10:50:26 +0300

    delete many files with endpoints

--- src/api/v1/auth/__init__.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-
-from src.api.v1.auth import signup_endpoint, login_endpoint
-
-__all__ = ["signup_endpoint", "login_endpoint"]
-auth_router = APIRouter(prefix="/auth")
