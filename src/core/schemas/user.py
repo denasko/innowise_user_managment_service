bd932375a9d703bb2b05b5d3fcaa51a604a41a3d
Avizhonis <aviz11@bk.ru>
2024-06-12 10:49:47 +0300

    update user schemas

--- src/core/schemas/user.py
+++ src/core/schemas/user.py
@@ -12,45 +12,26 @@ class UserBase(BaseModel):
     name: str
     surname: str
     username: str
-    password: str
     phone_number: str
     email: str
     role: Role = Role.USER
     image_s3_path: Optional[str] = None
-    is_blocked: bool = False
 
 
-class UserRead(BaseModel):
-    name: str
-    surname: str
-    username: str
-    phone_number: str
-    email: str
-    role: Role = Role.USER
+class UserRead(UserBase): ...
 
 
-class UserCreate(BaseModel):
-    name: str
-    surname: str
-    username: str
+class UserCreate(UserBase):
     password: str
-    phone_number: str
-    role: Role = Role.USER
-    image_s3_path: str
-    email: str
     group_id: UUID
 
 
-class UserUpdate(BaseModel):
-    name: str
-    surname: str
-    username: str
-    phone_number: str
-    email: str
+class UserUpdate(UserBase): ...
 
 
-class User(BaseModel):
+class User(UserBase):
     id: UUID
     created_at: datetime
     modified_at: datetime
+    is_blocked: bool = False
     group: Group
