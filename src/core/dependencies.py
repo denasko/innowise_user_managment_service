27193c451119277d52a52903962c6a7aa1276f02
Avizhonis <aviz11@bk.ru>
2024-06-13 14:35:08 +0300

    add new dependencies func

--- src/core/dependencies.py
+++ src/core/dependencies.py
@@ -1,10 +1,12 @@
 from fastapi import Depends
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.database.database import async_session
+from src.core.database.models.user import User
 from src.services.authorization_service import AuthService
-from src.services.user_service import UserService
 from src.services.token_sevice import TokenService
+from src.services.user_service import UserService
 
 
 async def get_session() -> AsyncSession:
@@ -24,3 +26,14 @@ def get_authorization_service(
     session: AsyncSession = Depends(get_session),
 ) -> AuthService:
     return AuthService(session=session)
+
+
+async def get_user_from_token(
+    credentials: HTTPAuthorizationCredentials = Depends(HTTPBearer),
+    auth_service: AuthService = Depends(get_authorization_service),
+) -> User:
+    payload: dict = auth_service.get_current_token_payload(credentials=credentials)
+
+    user_from_jwt: User = await auth_service.get_user_from_jwt(payload=payload)
+
+    return user_from_jwt
