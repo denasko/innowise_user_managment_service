7f77453d074fe00a6cd2181ac1a51aa67c5528e6
Avizhonis <aviz11@bk.ru>
2024-06-19 12:29:14 +0300

    fixed enums names and fields

--- src/core/database/enums/sorting.py
+++ src/core/database/enums/sorting.py
@@ -1,19 +1,19 @@
 import enum
 
 
-class OrderBY(enum.StrEnum):
+class OrderBy(enum.StrEnum):
     DESC = "desc"
     ASC = "asc"
 
 
-class SortBY(enum.StrEnum):
-    ID = ("id",)
-    NAME = ("name",)
-    SURNAME = ("surname",)
-    USERNAME = ("username",)
-    ROLE = ("role",)
-    IMAGE_S3_PATH = ("image_s3_path",)
-    IS_BLOCKED = ("is_blocked",)
-    EMAIL = ("email",)
-    PHONE_NUMBER = ("phone_number",)
-    GROUP_ID = ("group_id",)
+class SortBy(enum.StrEnum):
+    ID = "id"
+    NAME = "name"
+    SURNAME = "surname"
+    USERNAME = "username"
+    ROLE = "role"
+    IMAGE_S3_PATH = "image_s3_path"
+    IS_BLOCKED = "is_blocked"
+    EMAIL = "email"
+    PHONE_NUMBER = "phone_number"
+    GROUP_ID = "group_id"
