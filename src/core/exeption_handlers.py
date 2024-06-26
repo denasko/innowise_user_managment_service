f52798024c613b99393d22a07c5ea45e6c9980f7
Avizhonis <aviz11@bk.ru>
2024-06-26 21:15:13 +0300

    add new excaptions classes

--- src/core/exeption_handlers.py
+++ src/core/exeption_handlers.py
@@ -37,3 +37,13 @@ class UserNotFoundException(HTTPException):
 class RedisException(HTTPException):
     def __init__(self, detail: str = "Redis error occurred"):
         super().__init__(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=detail)
+
+
+class ValidationException(HTTPException):
+    def __init__(self, detail: str):
+        super().__init__(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=detail)
+
+
+class S3BucketException(HTTPException):
+    def __init__(self, detail: str = "S3 bucket not found"):
+        super().__init__(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail=detail)
